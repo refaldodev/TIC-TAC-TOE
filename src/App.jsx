e8066e9a504e1dc21d05fad5428df0660e84aa07
refaldodev <m.refaldorachman@gmail.com>
2026-02-04 21:10:12 +0700

    rotate warning

--- src/App.jsx
+++ src/App.jsx
@@ -91,6 +91,13 @@ function App() {
   }
   return (
     <main>
+     
+<div class="rotate-warning">
+  <div>
+    <h2>📱 Please Rotate Your Phone</h2>
+    <p>Use landscape mode for better experience</p>
+  </div>
+</div>
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
